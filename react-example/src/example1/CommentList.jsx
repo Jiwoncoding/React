17438a8e0fce930312864da693af12c2ba35a4c5
Jiwoncoding <seojiwonba@gmail.com>
2022-09-04 00:18:42 +0900

    댓글 컴포넌트 만들기 1차 작업 완료

---
+++ react-example/src/example1/CommentList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Commnet from './Comment';
+
+function CommentList(props){
+  return(
+    <>
+      <Commnet name={"서지원"} comment={"안녕하세요."} />
+    </>
+  );
+}
+
+export default CommentList;
