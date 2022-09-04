5a22990cd0e015ab5ab6e6f8eeaccae07f45b3cf
Jiwoncoding <seojiwonba@gmail.com>
2022-09-04 13:42:13 +0900

    example3 react custom hook 만들기

--- react-example/src/index.js
+++ react-example/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Accommodate from './example3/Accommodate';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import CommentList from './example1/CommentList';
-import NotificationList from './example2/NotificationList';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NotificationList />
+    <Accommodate />
   </React.StrictMode>
 );
 
