dfed3f8e973106c8414e2b34c319b6b1273cd411
Jiwoncoding <seojiwonba@gmail.com>
2022-09-09 13:26:46 +0900

    할 일 추가하기

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -41,6 +41,27 @@ export default class App extends Component {
     this.setState({todoData: newTodoData});
   };
 
+  handleChange = (e) => {
+    console.log('e', e.target.value);
+    this.setState({value: e.target.value});
+  }
+
+  handleSubmit = (e) => {
+    // form안에 input을 전송할 때 페이지 리로드되는 것을 막아줌
+    e.preventDefault();
+
+    // 새로운 할 일 데이터
+    let newTodo = {
+      id: Date.now(),
+      title: this.state.value,
+      completed: false,
+    };
+
+    // 원래 있던 할 일에 새로운 할 일 추가
+    this.setState({todoData: [...this.state.todoData, newTodo]});
+  };
+
+
   render() {
     return(
       <div className='container'>
@@ -59,6 +80,13 @@ export default class App extends Component {
           </p>
         </div>
           ))}
+
+          <form style={{display:'flex'}} onSubmit={this.handleSubmit}>
+            <input type="text" name="value" style={{flex:'10', padding: '5px'}} placeholder="할 일을 입력하세요"
+                  value={this.state.value} onChange={this.handleChange} />
+            <input type="submit" value="추가" className="btn" style={{flex: '1'}} />
+          </form>
+
         </div>
       </div>
     )
